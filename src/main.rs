6a83d0ae94cc2cbbe672e50b795667b6a51523d2
Aaron Landwehr <snaphat@gmail.com>
2021-07-24 02:19:11 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::time::Instant;
+
+fn libm_test() -> f32 {
+    let before = Instant::now();
+    let mut a = 0f32;
+    for _ in 0..0xFFFFFFF {
+        a += libm::cosf(1.5f32);
+    }
+    let after =  before.elapsed();
+    println!("libm Elapsed time: {:.2?}", after);
+    a as f32
+}
+
+fn micromath_test() -> f32 {
+    let before = Instant::now();
+    let mut a = 0f32;
+    for _ in 0..0xFFFFFFF {
+        a += micromath::F32::cos(1.5f32.into());
+    }
+    let after =  before.elapsed();
+    println!("micromath Elapsed time: {:.2?}", after);
+    a as f32
+}
+
+fn main()
+{
+    libm_test();
+    micromath_test();
+}
