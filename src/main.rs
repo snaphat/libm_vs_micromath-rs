beec20158dd2a6c6ae58b58760da25b7de7e1a25
Aaron Landwehr <snaphat@gmail.com>
2021-07-24 02:25:12 -0400

    Increase interations. Takes approximately 32.2s vs 16.16s on a ryzen x3900

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 fn libm_test() -> f32 {
     let before = Instant::now();
     let mut a = 0f32;
-    for _ in 0..0xFFFFFFF {
+    for _ in 0..0xFFFFFFFFu32 {
         a += libm::cosf(1.5f32);
     }
     let after =  before.elapsed();
@@ -14,7 +14,7 @@ fn libm_test() -> f32 {
 fn micromath_test() -> f32 {
     let before = Instant::now();
     let mut a = 0f32;
-    for _ in 0..0xFFFFFFF {
+    for _ in 0..0xFFFFFFFFu32 {
         a += micromath::F32::cos(1.5f32.into());
     }
     let after =  before.elapsed();
